b8c4e13fb5a30567ba72a1cddfa5ea32878a1cab
Kishen271828 <kg240904kg@gmail.com>
2024-03-04 19:06:21 +0800

    try-catch

--- foo.java
+++ foo.java
@@ -1,5 +1,9 @@
 public class foo {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        try {
+            System.out.println("hello world");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
